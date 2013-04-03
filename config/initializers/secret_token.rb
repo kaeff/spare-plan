bcbd4f3737429cfdc6b2ec234abc384f07fd1a2a
Klaus Fleerkötter <kaeff@gmx.net>
2013-04-03 12:31:28 +0200

    spare-plan | http://drone.bz

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SparePlan::Application.config.secret_token = 'a68cc854db0f9d5cfcccd35d76eddf20b69d23ae33015cdc1b537f772518599c14b566d4b4cfc59151870f6fd06b8e1b54cd3ca17d381d8c614c793c43c30409'
